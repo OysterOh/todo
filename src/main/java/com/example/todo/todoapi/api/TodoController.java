45e63354e667a27d325cc74a95f1a96eefb74364
jungwon <jungwon2433@naver.com>
2023-06-21 13:23:29 +0900

    # 1. todo api 할 일 기능 완성

---
+++ src/main/java/com/example/todo/todoapi/api/TodoController.java
@@ -0,0 +1,95 @@
+package com.example.todo.todoapi.api;
+
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
+import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
+import com.example.todo.todoapi.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    // 할 일 등록 요청
+    @PostMapping
+    public ResponseEntity<?> createTodo(@Validated @RequestBody TodoCreateRequestDTO requestDTO,
+                                        BindingResult result) {
+        if(result.hasErrors()) {
+            log.warn("DTO 검증 에러 발생: {}", result.getFieldErrors());
+            return ResponseEntity.badRequest().body(result.getFieldErrors());
+        }
+
+        try {
+            TodoListResponseDTO responseDTO = todoService.create(requestDTO);
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (RuntimeException e) {
+            log.error(e.getMessage());
+            return ResponseEntity
+                    .internalServerError()
+                    .body(TodoListResponseDTO.builder().error(e.getMessage()));
+        }
+
+
+    }
+
+    // 할 일 삭제 요청
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTodo(@PathVariable("id") String id) {
+        log.info("/api/todos/{} DELETE request", id);
+        if (id == null || id.trim().equals("")) {
+            return ResponseEntity.badRequest().body(TodoListResponseDTO.builder().error("ID를 전달해 주세요"));
+        }
+
+        try {
+        TodoListResponseDTO responseDTO = todoService.delete(id);
+        return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(TodoListResponseDTO.builder().error(e.getMessage()));
+        }
+
+    }
+
+    // 할 일 목록 요청
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        log.info("/api/todos GET request");
+        TodoListResponseDTO responseDTO = todoService.retrieve();
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
+    // 할 일 수정 요청
+    @RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<?> updateTodo(@Validated @RequestBody TodoModifyRequestDTO requestDTO,
+                                        BindingResult result,
+                                        HttpServletRequest request) {
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().body(result.getFieldErrors());
+        }
+
+        log.info("/api/todos {} request", request.getMethod());
+        log.info("modifying dto: {}", requestDTO);
+
+        try {
+            TodoListResponseDTO responseDTO = todoService.update(requestDTO);
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError()
+                    .body(TodoListResponseDTO.builder().error(e.getMessage()));
+        }
+
+    }
+
+}
